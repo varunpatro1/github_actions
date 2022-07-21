2d1cb19fd140633fdfe7f2857f79c339ac7026a0
Varun Patro <varunpatro@Varuns-MacBook-Pro.local>
2022-07-21 16:27:05 -0700

    added tests in test file

--- sum_and_product.py
+++ sum_and_product.py
@@ -1,18 +1,18 @@
 # Test File for github actions test coverage
 
-def sum(x,y):
+def get_sum(x,y):
+    """
+    Given two values, returns sum
+    """
+
     return x + y
 
-def product(x,y):
+def get_product(x,y):
+    """
+    Given two values, returns product
+    """
+    
     return x*y
 
-def main():
-    x = 4 
-    y = 5
-    assert(sum(x,y) == 9)
-    assert(product(x,y) == 20)
 
-    print('TESTS PASSED')
-
-main()
 
---
+++ test/test_sum_and_product.py
@@ -0,0 +1,11 @@
+#This file will contain the tests
+
+from sum_and_product import *
+
+def test_get_sum():
+    assert(get_sum(4,5) == 9)
+
+##def test_get_product():
+    ##assert(get_product(4,5) == 20)
+
+
