b2aeff4e006dcdccfb5ebfbbd33afa90922872a0
Varun Patro <varunpatro@Varuns-MacBook-Pro.local>
2022-07-22 10:33:07 -0700

    small changes

--- parent/test/test_sum_and_product.py
+++ parent/test/test_sum_and_product.py
@@ -5,8 +5,8 @@ from parent.proj.sum_and_product import *
 def test_get_sum():
     assert(get_sum(4,5) == 9)
 
-##def test_get_product():
-    ##assert(get_product(4,5) == 20)
+def test_get_product():
+    assert(get_product(4,5) == 20)
 
 
 
