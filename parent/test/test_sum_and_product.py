28d3077c0711ee99a5e9aea5e18be0427800ade5
Varun Patro <varunpatro@Varuns-MBP.lan>
2022-07-21 22:32:43 -0700

    added __init__.py files

--- parent/test/test_sum_and_product.py
+++ parent/test/test_sum_and_product.py
@@ -1,15 +1,17 @@
 #This file will contain the tests
-import sys
-import os
-sys.path.append('/Users/varunpatro/Desktop/github_actions_test')
 
-from sum_and_product import *
+from parent.proj.sum_and_product import *
 
 def test_get_sum():
     assert(get_sum(4,5) == 9)
 
 def test_get_product():
     assert(get_product(4,5) == 20)
+
+def main():
+    print(get_sum(4,5))
+
+main()
 
 
 
